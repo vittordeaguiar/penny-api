8e15542c2aa99dbeb97d446ee30e3aeab8fb7e2a
vittordeaguiar <vittoraguiar93@gmail.com>
2025-12-11 17:55:07 -0300

    first commit

---
+++ api/src/test/java/com/vittor/pennyapi/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittor.pennyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
